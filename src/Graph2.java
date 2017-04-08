92a3beed1c750af47668e5738596c52d79681ffd
susanoobit <susanoobit@users.noreply.github.com>
2017-04-08 01:51:35 -0300

    Finalização

--- src/Graph2.java
+++ src/Graph2.java
@@ -7,7 +7,12 @@ public class Graph2 extends Graph {
 		
 		ClosedLoop (int weight, int[] path) {
 			this.weight = weight;
-			this.path = path;
+			this.path = new int[path.length];
+			for (int i = 0; i < path.length; this.path[i] = path[i], i++);
+		}
+		
+		ClosedLoop () {
+			this.weight = 0;
 		}
 	}
 	
@@ -21,29 +26,77 @@ public class Graph2 extends Graph {
 	public Graph2(int num_vertices) {
 		super(num_vertices);
 	}
+	
+	ClosedLoop PCV(int start_vertice) throws Exception {
+		int visited_vertices[] = new int[num_vertices];
+		int full_path[] = new int[num_vertices + 1];
+		full_path[0] = start_vertice;
+		ClosedLoop best_path = checkVertice(start_vertice, visited_vertices, start_vertice, full_path, 1);
+		visited_vertices = null;
+		full_path = null;
+		return best_path;
+	}
+	
+	ClosedLoop checkVertice (int vertice, int visited_vertice[], int start_vertice, int full_path[], int qnt_elements) {
+		visited_vertice[vertice] = 1;
+		ClosedLoop best_path = new ClosedLoop(), tmp;
+
+		for (short i = 0; i < num_vertices; i++) {
+			if (matrix[vertice * num_vertices + i].edge == 1 && visited_vertice[i] == 0) {
+				full_path[qnt_elements] = i;
+				tmp = checkVertice(i, visited_vertice, start_vertice, full_path, qnt_elements + 1);
+				if (best_path.weight == 0 || tmp.weight < best_path.weight)
+					best_path = tmp;
+				full_path[qnt_elements] = -1;
+			}
+			else if (i == start_vertice &&
+					qnt_elements == num_vertices &&
+					matrix[i * num_vertices + vertice].edge == 1) {
+				full_path[qnt_elements] = start_vertice;
+				ClosedLoop found_path = new ClosedLoop(this.getPathWeight(full_path), full_path);
+				full_path[qnt_elements] = -1;
+				visited_vertice[vertice] = 0;
+				if (best_path.weight == 0 || found_path.weight < best_path.weight) {
+					best_path = null;
+					tmp = null;
+					return found_path;
+				}
+				else {
+					found_path = null;
+					tmp = null;
+					return best_path;
+				}
+			}
+		}
 		
-	void DFS2(int start_vertice) {
+		visited_vertice[vertice] = 0;
+		tmp = null;
+		return best_path;
+	}
+		
+	ClosedLoop[] PCV2(int start_vertice) throws Exception {
 		short visited_vertices[] = new short[num_vertices];
 		ArrayList<Integer> full_path = new ArrayList<Integer>(this.num_vertices + 1);
 		full_path.add(new Integer(start_vertice));
-		visit2(start_vertice, visited_vertices, start_vertice, full_path);
+		checkVertice2(start_vertice, visited_vertices, start_vertice, full_path);
 		this.closed_loops = new ClosedLoop[this.closed_loopsal.size()];
 		this.closed_loops = this.closed_loopsal.toArray(this.closed_loops);
 		this.closed_loopsal = new ArrayList<ClosedLoop>();
 		visited_vertices = null;
+		return getBestLoops();
 	}
 
-	void visit2 (int vertice, short visited_vertice[], int start_vertice, ArrayList<Integer> full_path) {
+	private void checkVertice2 (int vertice, short visited_vertice[], int start_vertice, ArrayList<Integer> full_path) {
 		visited_vertice[vertice] = 1;
 
 		for (int i = 0; i < num_vertices; i++) {
 			if (matrix[vertice * num_vertices + i].edge == 1 && visited_vertice[i] == 0) {
 				full_path.add(new Integer(i));
-				visit2(i, visited_vertice, start_vertice, full_path);
+				checkVertice2(i, visited_vertice, start_vertice, full_path);
 				full_path.remove(getLastPopulatedIndex(full_path));
 			}
 			else if (i == start_vertice &&
-					countNonZeroElements(full_path) == num_vertices - 1 &&
+					countNonNullElements(full_path) == num_vertices &&
 					matrix[i * num_vertices + vertice].edge == 1) {
 				full_path.add(new Integer(start_vertice));
 				int[] path = new int[full_path.size()];
@@ -51,9 +104,7 @@ public class Graph2 extends Graph {
 					path[j] = full_path.get(j).intValue();
 				}
 				this.closed_loopsal.add(new ClosedLoop(this.getPathWeight(path), path));
-				int lastIndex = getLastPopulatedIndex(full_path);
-				full_path.remove(lastIndex);
-				full_path.remove(lastIndex - 1);
+				full_path.remove(num_vertices);
 				visited_vertice[vertice] = 0;
 				return;
 			}
@@ -69,10 +120,11 @@ public class Graph2 extends Graph {
 		return weight;
 	}
 	
-	ClosedLoop getBestLoop() throws Exception {
+	ClosedLoop[] getBestLoops() throws Exception {
 		if (this.closed_loops.length == 0)
 			throw new Exception("There is no closed loop. Try to invoke the DFS2 method first and try again.");
 		
+		ArrayList<ClosedLoop> best_closed_loops = new ArrayList<ClosedLoop>();
 		ClosedLoop best_path = this.closed_loops[0];
 		int weight = this.getPathWeight(best_path.path);
 		for (int i = 1; i < this.closed_loops.length; i++) {
@@ -80,25 +132,36 @@ public class Graph2 extends Graph {
 			if (aux_weight < weight)
 				best_path = this.closed_loops[i];
 		}
-		return best_path;
+		for (int i = 0; i < this.closed_loops.length; i++) {
+			if (this.closed_loops[i].weight == best_path.weight)
+				best_closed_loops.add(this.closed_loops[i]);
+		}
+		return best_closed_loops.toArray(new ClosedLoop[0]);
 	}
 	
 	void printClosedLoop(ClosedLoop cl) {
-		System.out.println("Path: ");
+		System.out.print("\nPath: ");
 		for (int i = 0; i < cl.path.length; i++) {
 			System.out.print(cl.path[i] + " ");
 		}
-		System.out.println("Weight: " + cl.weight);
+		System.out.print("\nWeight: " + cl.weight);
 	}
 	
-	int countNonZeroElements(ArrayList<Integer> path) {
+	void printClosedLoopCSV(ClosedLoop cl) {
+		for (int i = 0; i < cl.path.length; i++) {
+			System.out.print(cl.path[i] + " ");
+		}
+		System.out.print("\t" + cl.weight);
+	}
+	
+	private int countNonNullElements(ArrayList<Integer> path) {
 		int length = 0;
 		for (int i = 0; i < path.size(); i++)
-			if (path.get(i).intValue() != 0) length++;
+			if (path.get(i) != null) length++;
 		return length;
 	}
 	
-	int getLastPopulatedIndex(ArrayList<Integer> path) {
+	private int getLastPopulatedIndex(ArrayList<Integer> path) {
 		for (int i = 0; i < path.size(); i++)
 			if (path.get(i) == null) return i - 1;
 		return path.size() - 1;
--- src/tp2_lab3.java
+++ src/tp2_lab3.java
@@ -1,37 +1,64 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
 
 public class tp2_lab3 {
+	// Gera um arquivo CSV
 	public static void main(String[] args) throws Exception {
-		FileReader fr = new FileReader("archive/k10.txt");
-		BufferedReader in = new BufferedReader(fr);
-		
-		int verticeQnt = Integer.parseInt(in.readLine().trim());
-		Graph2 graph = new Graph2(verticeQnt);
-		
-		String line = in.readLine();
-		while (line != null) {
-			String[] data = line.split("\t");
-			graph.addEdge(Integer.parseInt(data[0]), Integer.parseInt(data[1]), Integer.parseInt(data[2]));
-			line = in.readLine();
+		final File folder = new File("../archive");
+		System.out.print("nome da entrada\tmelhor caminho\tpeso\ttempo gasto (ns)");
+		for (final File file : folder.listFiles()) {
+			
+			FileReader fr = new FileReader(file);
+			BufferedReader in = new BufferedReader(fr);
+			
+			int verticeQnt = Integer.parseInt(in.readLine().trim());
+			Graph2 graph = new Graph2(verticeQnt);
+			
+			String line = in.readLine();
+			while (line != null) {
+				String[] data = line.split("\t");
+				graph.addEdge(Integer.parseInt(data[0]), Integer.parseInt(data[1]), Integer.parseInt(data[2]));
+				graph.addEdge(Integer.parseInt(data[1]), Integer.parseInt(data[0]), Integer.parseInt(data[2]));
+				line = in.readLine();
+			}
+			
+			long start = System.nanoTime();
+			Graph2.ClosedLoop best_loop = graph.PCV(0);
+			long elapsedTime = System.nanoTime() - start;
+			System.out.print("\n" + file.getName() + "\t");
+			graph.printClosedLoopCSV(best_loop);
+			System.out.print("\t" + elapsedTime);
+			graph = null;
+			best_loop = null;
+			
+			in.close();
 		}
 		
-		graph.print();
-		graph.DFS();
-		graph.DFS2(0);
-		System.out.println("Os caminhos existentes no grafo são:\n");
+		
+		
+		/*
+		Graph2.ClosedLoop[] best_loops = graph.PCV2(0);
+		long elapsedTime = System.currentTimeMillis() - start;
+		System.out.println("Os caminhos existentes no grafo são:");
 		for (int i = 0; i < graph.closed_loops.length; i++) {
-			System.out.println("Loop " + i);
+			System.out.print("\nLoop " + i);
 			graph.printClosedLoop(graph.closed_loops[i]);
-			System.out.println("");
+			System.out.print("\n");
 			
 		}
-		
-		Graph2.ClosedLoop best_loop = graph.getBestLoop();
-		System.out.println("\nO menor caminho é:");
-		graph.printClosedLoop(best_loop);
-		
-		in.close();
+		System.out.println("\n\n\n===========================================\n\n");
+		if (best_loops.length == 0) {
+			System.out.println("\nO menor caminho é:");
+			graph.printClosedLoop(best_loops[0]);
+		}
+		else {
+			System.out.println("\nOs " + best_loops.length + " menores caminhos são:");
+			for (int i = 0; i < best_loops.length; i++) {
+				graph.printClosedLoop(best_loops[i]);
+				System.out.print("\n");
+			}
+		}
+		*/	
 	}	
 }
