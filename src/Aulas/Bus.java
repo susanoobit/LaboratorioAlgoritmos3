884931d2b2c8c4c35e5b15677a4b901a5128c36a
Susanoo <george.melgaco@gmail.com>
2017-06-10 02:26:45 -0300

    Alguns testes com o problema de permutacao de onibus (cores e tamanhos). Sem sucesso.

--- src/Aulas/Bus.java
+++ src/Aulas/Bus.java
@@ -1,5 +1,92 @@
 package Aulas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Bus {
 	
+	private static int numberOfDigitsToShow = 6;
+	
+	private static class Problem {
+		
+		public final static long maxValue = (long) Math.pow(10, 15);
+		public final static int busLength = 10, mBusLength = 5;
+		long totalLength, busColorQuantity, mBusColorQuantity;
+		
+		Problem (long totalLength, long busColorQuantity, long mBusColorQuantity) {
+			this.totalLength = totalLength;
+			this.busColorQuantity = busColorQuantity;
+			this.mBusColorQuantity = mBusColorQuantity;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		readInput2().stream().forEach(Bus::solveProblem);
+	}
+	
+	private static void solveProblem (Problem problem) {
+		long arrangementQuantity = 0;
+		long maxNumberOfBus = problem.totalLength / Problem.busLength;
+		long maxNumberOfMBusAfterBus = problem.totalLength % Problem.busLength / Problem.mBusLength;
+		int relationBusMBus = Problem.busLength / Problem.mBusLength;
+		
+		for (long i = maxNumberOfBus, j = maxNumberOfMBusAfterBus; i > -1; i--, j += relationBusMBus) {
+			arrangementQuantity += fat(problem.busColorQuantity, i) * fat(problem.mBusColorQuantity, j) * (i != 0 && j != 0 ? fat(i + j) : 1);
+		}
+		
+		String print = String.valueOf(arrangementQuantity);
+		while (print.length() < numberOfDigitsToShow) print = "0" + print;
+		print = print.substring(print.length() - numberOfDigitsToShow);
+		System.out.println(print);
+	}
+	
+	private static long fat (long n, long maxMultiplications) {
+		if (n < 2 || maxMultiplications == 0) return 1;
+		return n * fat(n - 1, maxMultiplications - 1);
+	}
+	
+	private static long fat (long n) {
+		if (n < 2) return 1;
+		return n * fat(n - 1);
+	}
+		
+	private static ArrayList<Problem> readInput () throws Exception {
+		ArrayList<Problem> problems = new ArrayList<Problem>();
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextLine()) {
+			long[] input = Arrays.asList(sc.nextLine().split(" ")).stream().mapToLong(Long::valueOf).toArray();
+			if (input[0] < 5 || input[1] < 1 || input[2] < 1 || input[0] > Problem.maxValue ||
+					input[1] > Problem.maxValue || input[2] > Problem.maxValue || input[0] % 5 != 0)
+				throw new Exception("Invalid input");
+			
+			problems.add(new Problem(input[0], input[1], input[2]));
+		}
+		
+		sc.close();
+		return problems;
+	}
+	
+	private static ArrayList<Problem> readInput2 () throws Exception {
+		ArrayList<Problem> problems = new ArrayList<Problem>();
+		FileReader fr = new FileReader("archive/buss-in");
+		BufferedReader in = new BufferedReader(fr);
+		
+		String line = in.readLine();
+		while (line != null) {
+			long[] input = Arrays.asList(line.split(" ")).stream().mapToLong(Long::valueOf).toArray();
+			if (input[0] < 5 || input[1] < 1 || input[2] < 1 || input[0] > Problem.maxValue ||
+					input[1] > Problem.maxValue || input[2] > Problem.maxValue || input[0] % 5 != 0)
+				throw new Exception("Invalid input");
+			
+			problems.add(new Problem(input[0], input[1], input[2]));
+			line = in.readLine();
+		}
+		
+		in.close();
+		return problems;
+	}
 }
